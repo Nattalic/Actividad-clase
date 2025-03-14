3019c1cdce54b68433101ed0a7ab61b89bc79bce
Natt - <you@example.com>
2025-03-14 17:04:49 -0500

    apuntes

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+//1. Comentarios
+
+//
+//
+
+//2. Variables
+
+//Variable que puede cambiar su valor
+
+//Variable Let
+let variableLet ="hola"
+
+console.log(variableLet)
+
+variableLet = "Mundo :3"
+
+console.log(variableLet)
+
+variableLet = "Esto es una variable"
+
+console.log(variableLet)
+
+//Variable Const (cuando se repiten muchas cosas en el codigo)
+const constanteConts = "Eso es una constante"
+
+console.log(constanteConts)
+
+
+//Variable var (asignar valores/variables)
+var Esmayor = true
